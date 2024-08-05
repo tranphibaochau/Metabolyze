1bac363a8aa4bf3ad7f3636adf32b77a28c937b2
tranphibaochau <tranphibaochau@gmail.com>
2024-08-04 23:40:21 -0400

    Fixed bugs

--- crop_msi_images.py
+++ crop_msi_images.py
@@ -2,9 +2,12 @@ from PIL import Image
 import os
 from collections import defaultdict
 import sys
+import zipfile
+from pathlib import Path
+import shutil
 
 
-input_folder = sys.argv[1]
+input_file = sys.argv[1]
 x1 = int(sys.argv[2])
 x2 = int(sys.argv[3])
 x3 = int(sys.argv[4])
@@ -59,52 +62,81 @@ def find_non_black_pixels(img):
     return coords
 
 
-def crop_msi_images(x1, x2, x3, output_width, output_height):
-    # ensure coordinates are in correct order and within image bounds
-    if not (0 <= x1 < x2 < x3):
-        raise ValueError("X-coordinates must be in increasing order!")
-    files = os.listdir(input_folder)
+def crop_msi_images(x1=0, x2=0, x3=0, output_width=500, output_height=500):
+    if x1 != 0 and x2 != 0 and x3 != 0:
+        # ensure coordinates are in correct order and within image bounds
+        if not (0 <= x1 < x2 < x3):
+            raise ValueError("X-coordinates must be in increasing order!")
 
-    for image_file in files:
-        img_name = image_file.split("(")[0]  # get part of the input file name
+    current_dir = os.getcwd()
+    # extract all the file inside the zip file
+    with zipfile.ZipFile(input_file, 'r') as z_file:
+        input_folder = Path(f"{current_dir}/zip_folder")
+        os.makedirs(input_folder, exist_ok=True)
+        z_file.extractall(input_folder)
+    # list all image file inside the new extracted folder
+    original_width, original_height = output_width, output_height
+    for image_file in input_folder.rglob('*.png'):
+        img_name = image_file.name.split("(")[0]  # get part of the input file name
         img_name = img_name.split(".png")[0]
         # first, crop out the blank region and metadata of the images
         with Image.open(os.path.join(input_folder, image_file)) as img:
             width, height = img.size
-            if x3 < width:
+            if x3 > width:
                 raise ValueError("X-coordinates must within image width bounds!")
+            # if x1, x2, x3 are specified, cut the images into parts
+            if x1 != 0 and x2 != 0 and x3 != 0:
+                boxes = [
+                    (0, 0, x1, height),
+                    (x1, 0, x2, height),
+                    (x2, 0, x3, height),
+                    (x3, 0, width, height)
+                ]
+                output_paths = []
+                for i in range(1, 5):
+                    output_paths.append(os.path.join(f"{os.getcwd()}/output/crop_msi_images/", f"{img_name}_s{i}.png"))
+                # crop and save
+                for box, output_path in zip(boxes, output_paths):
+                    cropped_img = img.crop(box)
+                    coords = find_non_black_pixels(cropped_img)
+                    # crop the images based on its non-black pixels
+                    left = coords["left_most"][0] - 20
+                    top = coords["top_most"][1] - 20
+                    right = coords["right_most"][0] + 20
+                    bottom = coords["bottom_most"][1] + 20
+                    cropped_img = cropped_img.crop((left, top, right, bottom))
 
-            # define the bounding boxes for the four segments
-            boxes = [
-                (0, 0, x1, height),
-                (x1, 0, x2, height),
-                (x2, 0, x3, height),
-                (x3, 0, width, height)
-            ]
-            output_paths = []
-            for i in range(1, 5):
-                output_paths.append(os.path.join(f"{os.getcwd()}/output/crop_msi_images/", f"{img_name}_s{i}.png"))
-            # crop and save
-            for box, output_path in zip(boxes, output_paths):
-                cropped_img = img.crop(box)
-                coords = find_non_black_pixels(cropped_img)
+                    # if cropped image is bigger than the specified size, don't add black background to the image
+                    size = max(output_width, output_height, cropped_img.width, cropped_img.height)
+                    new_image = Image.new('RGB', (size, size), (0, 0, 0))
+                    # Calculate the position to paste the original image onto the new image
+                    paste_position = (
+                        (size - cropped_img.width) // 2, (size - cropped_img.height) // 2)
+                    # Paste the original image onto the new image
+                    new_image.paste(cropped_img, paste_position)
+                    new_image = new_image.resize((output_width, output_height))
+                    new_image.save(output_path)
+            else:
+                coords = find_non_black_pixels(img)
                 # crop the images based on its non-black pixels
                 left = coords["left_most"][0] - 20
                 top = coords["top_most"][1] - 20
                 right = coords["right_most"][0] + 20
                 bottom = coords["bottom_most"][1] + 20
-                cropped_img = cropped_img.crop((left, top, right, bottom))
-                # if cropped image is larger than output width and output height, it will override the size
-                if cropped_img.width > output_width or cropped_img.height > output_height:
-                    cropped_img.save(output_path)
-                else:
-                    new_image = Image.new('RGB', (output_width, output_height), (0, 0, 0))
-                    # Calculate the position to paste the original image onto the new image
-                    paste_position = ((output_width - cropped_img.width) // 2, (output_height - cropped_img.height) // 2)
-                    # Paste the original image onto the new image
-                    new_image.paste(cropped_img, paste_position)
-                    new_image.save(output_path)
-
+                cropped_img = img.crop((left, top, right, bottom))
+                # remember the image size we want to save
+                # if cropped image is bigger than the specified size, don't add black background to the image
+                size = max(output_width, output_height, cropped_img.width, cropped_img.height)
+                new_image = Image.new('RGB', (size, size), (0, 0, 0))
+                # Calculate the position to paste the original image onto the new image
+                paste_position = (
+                (size - cropped_img.width) // 2, (size - cropped_img.height) // 2)
+                # Paste the original image onto the new image
+                new_image.paste(cropped_img, paste_position)
+                new_image = new_image.thumbnail((500, 500))
+                print(f"Resized size: {new_image.size}")
+                new_image.save(f"{os.getcwd()}/output/crop_msi_images/{img_name}.png")
+    shutil.rmtree(input_folder)  # remove the unzipped folder afterwards
 # Example usage
-crop_msi_images(x1, x2, x3, 500, 500)
+crop_msi_images(x1, x2, x3, output_width, output_height)
 
